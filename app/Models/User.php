d68cdefa9b5f45c5900abda32c746a887cafe365
jimmyazji <jimmyyaji98@gmail.com>
2023-01-03 14:41:22 +0300

    added crud operations (backend)

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -73,8 +74,26 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    
+
     protected $appends = [
         'name',
     ];
+
+    public function resolveRouteBinding($value, $field = null)
+    {
+        return $this->where('username', $value)->firstOrFail();
+    }
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn ($query, $search) =>
+        $query->where(
+            fn ($query) => $query->where('first_name', 'LIKE', '%' . $search . '%')
+                ->orWhere('last_name', 'LIKE', '%' . $search . '%')
+                ->orWhere('email', 'LIKE', '%' . $search . '%')
+                ->orWhere('username', 'LIKE', '%' . $search . '%')
+                ->orWhere(DB::raw("concat(first_name, ' ', last_name)"), 'LIKE', "%" . $search . "%")
+                ->orWhere(DB::raw("concat(last_name, ' ', first_name)"), 'LIKE', "%" . $search . "%")
+        ));
+    }
 }
